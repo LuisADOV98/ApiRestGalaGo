24886175f337a8ce37d220d5a65e8c409dd7d7d5
LuisADOV98 <luisadov26@gmail.com>
2023-09-25 18:53:17 +0200

    vercel create

--- src/database.js
+++ src/database.js
@@ -5,6 +5,6 @@ const connection = mysql.createConnection(
         host: process.env.DB_HOST || "localhost",
         user: process.env.DB_USER || "admin",
         password: process.env.DB_PASSWORD || "GalaGo2023",
-        database: process.env.DB_NAME || "GalaGo",
+        database: process.env.DB_NAME || "galaGo",
         port: process.env.DB_PORT || 3306
     })
