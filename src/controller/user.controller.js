36950c0880142bc0df95359d7883eee9c6e25aed
LuisADOV98 <luisadov26@gmail.com>
2023-09-26 11:35:10 +0200

    Creado endpoint punto  de inicio

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,12 @@
 const {pool} = require("../database");
 const Prenda = require("../models/user");
 
+function getStart(request, response)
+{
+    let respuesta = {error: true, codigo:200, mensaje: "Punto de inicio"}
+    response.send(respuesta)
+}
 
 
 
-
-module.exports = {}
+module.exports = {getStart}
--- src/database.js
+++ src/database.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection(
+const pool = mysql.createPool(
     {
-        host: process.env.DB_HOST || "localhost",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASSWORD || "",
+        host: process.env.DB_HOST || "galago.cnenmyr1gswr.eu-north-1.rds.amazonaws.com",
+        user: process.env.DB_USER || "admin",
+        password: process.env.DB_PASSWORD || "GalaGo.2023",
         database: process.env.DB_NAME || "GalaGo",
         port: process.env.DB_PORT || 3306
-    })
+    }).promise();
+    console.log("conexion con la BBDD Creada");
+
+    module.exports = {pool}
 
