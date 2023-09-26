0deaf6d9289571a499d778444e5d7361d7840f95
LuisADOV98 <luisadov26@gmail.com>
2023-09-26 20:08:14 +0200

    landing incompleta

--- src/controller/prenda.controller.js
+++ src/controller/prenda.controller.js
@@ -1,7 +1,23 @@
 const {pool} = require("../database");
 const Prenda = require("../models/prenda");
 
+const getPrenda = async (request, response) =>
+{
+    try
+    {
+        let sql = "SELECT * FROM prenda WHERE idprenda <= 4";
 
 
+        let [result] = await pool.query(sql);
+        let respuesta = {error:false, codigo:200,
+            mensaje:"estos son las prendas", data:result}
+        response.send(respuesta);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
 
-module.exports = {}
+
+module.exports = {getPrenda}
--- src/routers/prenda.routers.js
+++ src/routers/prenda.routers.js
@@ -2,6 +2,6 @@ const {Router} = require('express');
 const router = Router();
 const prendaCtrl = require("../controller/prenda.controller");
 
-
+router.get("/landing-page", prendaCtrl.getPrenda)
 
 module.exports = router;
