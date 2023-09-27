bc7655826281d23afd0448ab8332152c6b2332ae
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-09-27 18:28:26 +0200

    ...

--- src/routers/prenda.routers.js
+++ src/routers/prenda.routers.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const prendaCtrl = require("../controller/prenda.controller");
 
-router.get("/landing-page", prendaCtrl.getPrenda); //muestra las 20 últimas prendas
+router.get("/prendas20", prendaCtrl.getPrenda); //muestra las 20 últimas prendas
 
 router.put("/editar-prenda", prendaCtrl.editarPrenda); //edita una prenda por idprenda y iduser
 
