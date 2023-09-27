4e158cd965954c6eeb0cd9e37174c0f0e3c74d91
LuisADOV98 <luisadov26@gmail.com>
2023-09-27 16:32:45 +0200

    nada

--- src/controller/prenda.controller.js
+++ src/controller/prenda.controller.js
@@ -1,16 +1,11 @@
 const { pool } = require("../database");
 const Prenda = require("../models/prenda");
 
-<<<<<<< HEAD
-const getPrenda = async (request, response) => {
-    try {
-=======
 //Muestra las 20 ultimas prendas de la BBDD
 const getPrenda = async (request, response) =>
 {
     try
     {
->>>>>>> 26e044234a873e84efff7f55d09281a5407176a1
         let sql = "SELECT * FROM prenda order by idprenda desc limit 20";
 
         let [result] = await pool.query(sql);
