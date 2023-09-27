a499ce5d3b1635bda0db080a91e9e2b05fa88c10
LuisADOV98 <luisadov26@gmail.com>
2023-09-27 12:35:32 +0200

    editar listo

--- src/controller/prenda.controller.js
+++ src/controller/prenda.controller.js
@@ -5,7 +5,7 @@ const getPrenda = async (request, response) =>
 {
     try
     {
-        let sql = "SELECT * FROM prenda order by idprenda desc limit 4 ;";
+        let sql = "SELECT * FROM prenda order by idprenda desc limit 20";
 
 
         let [result] = await pool.query(sql);
@@ -20,4 +20,61 @@ const getPrenda = async (request, response) =>
 }
 
 
-module.exports = {getPrenda}
+const editarPrenda = async (request, response) => {
+    try {
+        console.log(request.body);
+      let params = [request.body.title,
+                    request.body.price,
+                    request.body.description,
+                    request.body.location,
+                    request.body.state,
+                    request.body.size,
+                    request.body.event,
+                    request.body.type,
+                    request.body.photo1,
+                    request.body.photo2,
+                    request.body.photo3,
+                    request.body.photo4,
+                    request.body.idprenda]
+
+        let sql = "UPDATE prenda SET title = COALESCE(?, title), " +
+                                     "price = COALESCE(?, price), " +
+                                     "description = COALESCE(?, description), " +
+                                     "location = COALESCE(?, location), " +
+                                     "state = COALESCE(?, state), " +
+                                     "size = COALESCE(?, size), " +
+                                     "event = COALESCE(?, event), " +
+                                     "type = COALESCE(?, type), " +
+                                     "photo1 = COALESCE(?, photo1), " +
+                                     "photo2 = COALESCE(?, photo2), " +
+                                     "photo3 = COALESCE(?, photo3), " +
+                                     "photo4 = COALESCE(?, photo4) " +
+                                     "WHERE idprenda = ?"
+      // Consulta SQL para actualizar la información de la prenda
+  
+      // Ejecutar la consulta SQL
+      let [result] = await pool.query(sql, params);
+  
+        console.log(result);
+      if(result.affectedRows > 0){
+      let respuesta = {error:false, codigo:200,
+          mensaje:"Se ha editado la prenda", dataUser:result}
+      response.send(respuesta);
+  }
+  else 
+  {
+      let respuesta = {error:true, codigo:400,
+          mensaje:"algo ha salido mal", dataUser:result}
+      response.send(respuesta);
+  }
+
+}
+catch(error)
+{
+  console.log(error);
+}
+  };
+
+
+
+module.exports = {getPrenda, editarPrenda}
--- src/routers/prenda.routers.js
+++ src/routers/prenda.routers.js
@@ -2,6 +2,10 @@ const {Router} = require('express');
 const router = Router();
 const prendaCtrl = require("../controller/prenda.controller");
 
-router.get("/landing-page", prendaCtrl.getPrenda)
+router.get("/landing-page", prendaCtrl.getPrenda);
+
+router.put("/editar-prenda", prendaCtrl.editarPrenda);
+
+
 
 module.exports = router;
