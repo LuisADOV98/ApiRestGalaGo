0e02c34a43d731a5d59aab202e5b1e1af35a5f2d
JanetRW <jane_row@hotmail.com>
2023-09-27 17:39:02 +0200

    subiendo chatcontroller

--- src/routers/chat.routers.js
+++ src/routers/chat.routers.js
@@ -3,6 +3,7 @@ const router = Router();
 const chatCtrl = require("../controller/chat.controller");
 
 
-
+router.get("/chat", chatCtrl.getChat);
+// router.delete("/chat", chatCtrl.delChat);
 
 module.exports = router;
