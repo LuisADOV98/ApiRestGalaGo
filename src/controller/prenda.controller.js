6bc85b4de72527decdbd789f317ab735e6fc8273
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-09-28 10:22:30 +0200

    enum ok

--- src/controller/prenda.controller.js
+++ src/controller/prenda.controller.js
@@ -19,6 +19,108 @@ const getPrenda = async (request, response) =>
     }
 }
 
+const getPrendaHome = async (request, response) =>
+{
+    try
+    {
+        let sql = "SELECT * FROM prenda order by idprenda desc limit 20";
+
+        let [result] = await pool.query(sql);
+        let respuesta = {error:false, codigo:200,
+            mensaje:"estos son las prendas", data:result}
+        response.send(respuesta);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+// ARRAYS DE ENUMS PARA FILTROS 
+const getTipo = async(req, res) => {
+    try{
+        let sql = "SELECT COLUMN_TYPE " +
+                  "FROM information_schema.COLUMNS " +
+                  "WHERE TABLE_SCHEMA = 'GalaGo' " +
+                  "AND TABLE_NAME = 'prenda' " +
+                  "AND COLUMN_NAME = 'type' "
+
+        let [result] = await pool.query(sql);
+        //Cojo sólo lo que esté entre comillas y coma para dejar un array de las opciones
+            //COLUMN_TYPE es para acceder a los datos del enum
+            //g define las barras /.../
+            //despues de coger las coincidencias .map devuelve los valores eliminando (slice) los caracteres 
+        let enumOptions = result[0].COLUMN_TYPE.match(/'([^']+)'/g).map(option => option.slice(1,-1));
+        //Devuelve un dataEnum porque es un array de string en la respuesta de Angular 
+        respuesta = {error: false, codigo: 200, dataEnum: enumOptions}
+        res.send(respuesta);
+    }catch(err){
+        console.log(err);
+    }
+}
+const getTalla = async(req, res) => {
+    try{
+        let sql = "SELECT COLUMN_TYPE " +
+                  "FROM information_schema.COLUMNS " +
+                  "WHERE TABLE_SCHEMA = 'GalaGo' " +
+                  "AND TABLE_NAME = 'prenda' " +
+                  "AND COLUMN_NAME = 'size' "
+
+        let [result] = await pool.query(sql);
+        //Cojo sólo lo que esté entre comillas y coma para dejar un array de las opciones
+            //COLUMN_TYPE es para acceder a los datos del enum
+            //g define las barras /.../
+            //despues de coger las coincidencias .map devuelve los valores eliminando (slice) los caracteres 
+        let enumOptions = result[0].COLUMN_TYPE.match(/'([^']+)'/g).map(option => option.slice(1,-1));
+        //Devuelve un dataEnum porque es un array de string en la respuesta de Angular 
+        respuesta = {error: false, codigo: 200, dataEnum: enumOptions}
+        res.send(respuesta);
+    }catch(err){
+        console.log(err);
+    }
+}
+const getEvento = async(req, res) => {
+    try{
+        let sql = "SELECT COLUMN_TYPE " +
+                  "FROM information_schema.COLUMNS " +
+                  "WHERE TABLE_SCHEMA = 'GalaGo' " +
+                  "AND TABLE_NAME = 'prenda' " +
+                  "AND COLUMN_NAME = 'event' "
+
+        let [result] = await pool.query(sql);
+        //Cojo sólo lo que esté entre comillas y coma para dejar un array de las opciones
+            //COLUMN_TYPE es para acceder a los datos del enum
+            //g define las barras /.../
+            //despues de coger las coincidencias .map devuelve los valores eliminando (slice) los caracteres 
+        let enumOptions = result[0].COLUMN_TYPE.match(/'([^']+)'/g).map(option => option.slice(1,-1));
+        //Devuelve un dataEnum porque es un array de string en la respuesta de Angular 
+        respuesta = {error: false, codigo: 200, dataEnum: enumOptions}
+        res.send(respuesta);
+    }catch(err){
+        console.log(err);
+    }
+}
+const getEstado = async(req, res) => {
+    try{
+        let sql = "SELECT COLUMN_TYPE " +
+                  "FROM information_schema.COLUMNS " +
+                  "WHERE TABLE_SCHEMA = 'GalaGo' " +
+                  "AND TABLE_NAME = 'prenda' " +
+                  "AND COLUMN_NAME = 'state' "
+
+        let [result] = await pool.query(sql);
+        //Cojo sólo lo que esté entre comillas y coma para dejar un array de las opciones
+            //COLUMN_TYPE es para acceder a los datos del enum
+            //g define las barras /.../
+            //despues de coger las coincidencias .map devuelve los valores eliminando (slice) los caracteres 
+        let enumOptions = result[0].COLUMN_TYPE.match(/'([^']+)'/g).map(option => option.slice(1,-1));
+        //Devuelve un dataEnum porque es un array de string en la respuesta de Angular 
+        respuesta = {error: false, codigo: 200, dataEnum: enumOptions}
+        res.send(respuesta);
+    }catch(err){
+        console.log(err);
+    }
+}
 //Edita una prenda por idprenda y iduser
 const editarPrenda = async (request, response) => {
     try {
@@ -76,4 +178,4 @@ catch(error)
   };
 
 
-module.exports = {getPrenda, editarPrenda}
+module.exports = {getPrenda, editarPrenda,getPrendaHome,getEstado,getEvento,getTalla,getTipo}
--- src/routers/prenda.routers.js
+++ src/routers/prenda.routers.js
@@ -2,7 +2,13 @@ const {Router} = require('express');
 const router = Router();
 const prendaCtrl = require("../controller/prenda.controller");
 
-router.get("/prendas20", prendaCtrl.getPrenda); //muestra las 20 últimas prendas
+router.get("/landing-page", prendaCtrl.getPrenda); //muestra las 20 últimas prendas
+router.get("/home", prendaCtrl.getPrendaHome); //muestra las 20 últimas prendas
+// RUTAS PARA EXTRAER ARRAY DE ENUM 
+router.get("/tipo", prendaCtrl.getTipo); //muestra las 20 últimas prendas
+router.get("/talla", prendaCtrl.getTalla); //muestra las 20 últimas prendas
+router.get("/evento", prendaCtrl.getEvento); //muestra las 20 últimas prendas
+router.get("/estado", prendaCtrl.getEstado); //muestra las 20 últimas prendas
 
 router.put("/editar-prenda", prendaCtrl.editarPrenda); //edita una prenda por idprenda y iduser
 
