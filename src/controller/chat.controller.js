a28fdcb5f654333fdcebbe50d29e0f5dc2e5e48b
JanetRW <jane_row@hotmail.com>
2023-10-01 07:28:23 +0200

    se implementan cambios en el chats

--- src/controller/chat.controller.js
+++ src/controller/chat.controller.js
@@ -16,10 +16,7 @@ const getChat =async(req,res)=>{
         let [resultado] = await pool.query(sql,params);
             if(resultado.length > 0){
                 res.send(resultado);
-          
-
             }else
-            
             {
                 res.send("No se encuentran chats ");
                 // respuesta = {error: true, codigo: 200,mensaje:"No se encuentran usuarios", res_chat: resultado};
@@ -28,96 +25,103 @@ const getChat =async(req,res)=>{
     catch (error) {
             res.send(error);
             }
-      
-        
-        // res.send(respuesta); 
-   
+ 
 }
 
+const verificarChat = async (req, res)=> {
+  try
+    {
+      const{iduser} =req.body;
+      let params3 =[iduser,iduser,iduser,iduser];
+      let sql3=`SELECT idchat FROM chat WHERE (iduser1 = ? AND iduser2 = ?) OR (iduser1 = ? AND iduser2 = ?) LIMIT 1;`;
+      let [resultado3] = await pool.query(sql3,params3);
+      if(resultado3.length > 0){
+          res.send(resultado3);
+      }else
+      {
+          res.send("No se encuentran chats ");
+          // respuesta = {error: true, codigo: 200,mensaje:"No se encuentran usuarios", res_chat: resultado};
+      }
+  } 
+catch (error) {
+      res.send(error);
+      }
+
+}
+
+const createMensaje = async (req, res) => {
+    try {
+    //   const { iduser1, iduser2, message } = req.body;
+      const { iduser1, iduser2, message , datehour, hasnewmessage,idchat} = req.body;
+
+      // Insertar un nuevo mensaje en la tabla de mensaje
+    //   let sql1 = `INSERT INTO mensaje (iduser, message) VALUES (?, ?)`;
+      let sql1 = `INSERT INTO mensaje (iduser, message,idchat) VALUES (?, ?,?)`;
+
+      let messageParams = [iduser1, message,idchat];
+      let [resultado1] = await pool.query(sql1, messageParams);
+  
+      let idmessage = resultado1.insertId;
+  
+      // Crear una nueva entrada en la tabla de chats
+      let sql2 = `INSERT INTO chat (datehour,hasnewmessage,iduser1, iduser2) VALUES (?, ?, ?,?)`;
+      let chatParams = [datehour,hasnewmessage,iduser1, iduser2];
+    //   let chatParams = [datehour,hasnewmessage,iduser1, iduser2,idmessage];
+
+      await pool.query(sql2, chatParams);
+  
+      res.status(201).json({ message: 'Conversación creada exitosamente' });
+    } catch (error) {
+      console.error('Error al crear conversación: ' + error.message);
+      res.status(500).json({ error: 'Error al crear la conversación' });
+    }
+  };
+
+  const getMensaje = async (req, res) => {
+    try {
+      const { iduser1, iduser2 } = req.query;
+  
+      // Consulta SQL para obtener los mensajes entre dos usuarios y los detalles de usuario
+      let sql = `
+        SELECT m.idmessage, m.message, m.iduser, m.idchat, u.firstname, u.photo
+        FROM mensaje AS m
+        JOIN user AS u ON m.iduser = u.iduser
+        JOIN chat AS c ON m.idchat = c.idchat
+        WHERE (c.iduser1 = ? AND c.iduser2 = ?) OR (c.iduser1 = ? AND c.iduser2 = ?)
+        ORDER BY m.idmessage ASC;
+      `;
+  
+      let queryParams = [iduser1, iduser2, iduser2, iduser1];
+  
+      let [resultado] = await pool.query(sql, queryParams);
+  
+      res.status(200).json(resultado);
+    } catch (error) {
+      console.error('Error al obtener mensajes: ' + error.message);
+      res.status(500).json({ error: 'Error al obtener mensajes' });
+    }
+  };
+  
 
 
-// const getChat =async(req,res)=>
-// {
-//     try{
-//         let sql;
-//         let respuesta;
-//         if(req.body.iduser!=null){
-//         sql = `SELECT * FROM chat WHERE iduser1=?` + res.body.iduser;
-//         let [resultado] = await pool.query(sql);
-//         respuesta = {error: false, codigo: 200,mensaje:"Estos son los chats", res_chat: resultado};
-//         console.log(resultado);
-//     }
-//         else{
-//         sql=`iduser no encontrado`;
-//         }
-        
-//         res.send(respuesta); 
-        
-//     }
-//     catch(err)
-//     {
-//         console.log(err);
-//     }
-// }
-
-
-
-// async function getChat(req,res){
-//     const {id_user,idchat} = req.query;
-//     const params = [id_user,idchat];
+//CONVERSACION-CHAT
+// async function postChat(req,res){
+//     const {idchat, photo, firstname, datehour, id_prenda, iduser} = req.body;
+//     const params = [idchat, photo, firstname, datehour, id_prenda, iduser];
+//     let sql= `INSERT INTO chat (idchat, photo, firstname, datehour, id_prenda, iduser) VALUES (?,?,?,?,?,?);`;
 //     let answer;
-//     let sql;
-//     //-- Definir las peticiones dependiendo de los dos casos q se pueden dar
-//     if (id_user != undefined && idchat == undefined) {
-//         sql = `SELECT * FROM chat WHERE id_user = ?;`;
-//     }else{
-//         sql = `SELECT * FROM chat WHERE id_user = ? AND idchat = ?;`;
-//     }
 
 //     try {
-//         const [respuesta] = await pool.query(sql,params);
-//         if (respuesta.length === 0) {
-//             answer = {error: true, code: 200, message: "Chat not found", data:null, result:respuesta};
+//         const [data] = await pool.query(sql,params);
+//         if (data.length === 0) {
+//             answer = {error: true, code: 200, message: "Registration error", data:data, result:null};
 //         }else{
-//             answer = {error: false, code: 200, message: "Chat found", data:null, result:respuesta};
+//             answer = {error: false, code: 200, message: "Chat registered correctly", data:data, result:null};
 //         }
 //         res.send(answer);
 //     } catch (error) {
 //         res.send(error)
 //     }
 // }
-
-// async function delChat(req,res){ 
-//     //const {idChat} = req.body;
-//     // let params = [idChat];
-//     let params = [req.body.idChat];
-//     let sql = `DELETE FROM chat WHERE idChat = ?;`;
-
-//     try {
-//         const [result] = await pool.query(sql,params);
-//         res.send(result);
-//     } catch (error) {
-//         res.send(error)
-//     }
-// }
-//CONVERSACION-CHAT
-async function postChat(req,res){
-    const {idchat, photo, firstname, datehour, id_prenda, iduser} = req.body;
-    const params = [idchat, photo, firstname, datehour, id_prenda, iduser];
-    let sql= `INSERT INTO chat (idchat, photo, firstname, datehour, id_prenda, iduser) VALUES (?,?,?,?,?,?);`;
-    let answer;
-
-    try {
-        const [data] = await pool.query(sql,params);
-        if (data.length === 0) {
-            answer = {error: true, code: 200, message: "Registration error", data:data, result:null};
-        }else{
-            answer = {error: false, code: 200, message: "Chat registered correctly", data:data, result:null};
-        }
-        res.send(answer);
-    } catch (error) {
-        res.send(error)
-    }
-}
 // module.exports = {getChat,delChat}
-module.exports = {getChat, postChat}
+module.exports = {getChat,verificarChat, createMensaje,getMensaje}
--- src/routers/chat.routers.js
+++ src/routers/chat.routers.js
@@ -3,8 +3,10 @@ const router = Router();
 const chatCtrl = require("../controller/chat.controller");
 
 
-router.get("/chat", chatCtrl.getChat);
-router.post("/chat", chatCtrl.postChat);
-// router.delete("/chat", chatCtrl.delChat);
+router.get("/chat", chatCtrl.getChat); //para mostrar la lista de chat
+router.post("/chat", chatCtrl.createMensaje);//para crear un nuevo mensaje dentro de la lista de chat
+router.get("/chat", chatCtrl.verificarChat);//
+router.get("/conversacion",chatCtrl.getMensaje);//para mostrar los mensajes de una conversacion-chat determinado
+// router.post("/chat", chatCtrl.postChat);
 
 module.exports = router;
